f9497a7d7537ae73b869e5daa4d32dfeadcb6f64
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-03-13 16:52:55 -0400

    Feat: transalate to spanish

--- web/models.py
+++ web/models.py
@@ -7,27 +7,27 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 class Config(SingletonModel):
 
-    business_name = models.CharField(_("business name"), max_length=50, blank=True, null=True)
-    business_name_active = models.BooleanField(_("business name in title"), default=False)
-    logo = models.ImageField(_("logo"), upload_to='config/', blank=True, null=True)
+    business_name = models.CharField(_("Nombre de negocio"), max_length=50, blank=True, null=True)
+    business_name_active = models.BooleanField(_("Nombre de negocio activo"), default=False)
+    logo = models.ImageField(_("Logo"), upload_to='config/', blank=True, null=True)
     facebook = models.URLField(_("facebook"), max_length=240, blank=True, null=True)
     twitter = models.URLField(_("twitter"), max_length=240, blank=True, null=True)
     telegram = models.URLField(_("telegram"), max_length=240, blank=True, null=True)
     pinterest = models.URLField(_("pinterest"), max_length=240, blank=True, null=True)
     flirk = models.URLField(_("flirk"), max_length=240, blank=True, null=True)
-    suscribe_title = models.CharField(_("suscribe title"), max_length=150, blank=True, null=True)
-    suscribe_text = models.TextField(_("suscribe text"),blank=True, null=True)
+    suscribe_title = models.CharField(_("Titulo suscripción"), max_length=150, blank=True, null=True)
+    suscribe_text = models.TextField(_("Texto suscripción"),blank=True, null=True)
 
     class Meta:
-        verbose_name = _("configuration")
+        verbose_name = _("Configuración")
 
 class Suscriptor(models.Model):
 
-    email = models.EmailField(_("email"), max_length=254, unique=True)
+    email = models.EmailField(_("Email"), max_length=254, unique=True)
 
     class Meta:
-        verbose_name = _("Suscriptor")
-        verbose_name_plural = _("Suscriptors")
+        verbose_name = _("Subscriptor")
+        verbose_name_plural = _("Subscriptores")
 
     def __str__(self):
         return self.email
@@ -37,12 +37,12 @@ class Suscriptor(models.Model):
 
 class New(models.Model):
 
-    title = models.CharField(_("title"), max_length=100)
-    news = RichTextField('news')
+    title = models.CharField(_("Titulo"), max_length=100)
+    news = RichTextField('Noticias')
 
     class Meta:
-        verbose_name = _("new")
-        verbose_name_plural = _("news")
+        verbose_name = _("Noticia")
+        verbose_name_plural = _("Noticias")
 
     def __str__(self):
         return self.news
